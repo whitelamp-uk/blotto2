b668a6879cf29d672b1ac1e129e1fc344c181851
Mark Page <mark.page@burdenandburden.co.uk>
2021-02-27 17:27:16 +0000

    Fixes bug

--- scripts/manual.php
+++ scripts/manual.php
@@ -38,7 +38,7 @@ else {
         }
     }
     if (!$group) {
-        fwrite (STDERR,"    Number-match group '$group' has no prizes\n");
+        fwrite (STDERR,"    Number-match group '$g' has no prizes\n");
         exit (105);
     }
     if (array_key_exists($group,$draw->results)) {
