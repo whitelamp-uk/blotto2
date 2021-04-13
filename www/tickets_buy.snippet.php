6d1eb58a5e6861e5a5ae56741cf11fc2edc82fd0
Mark Page <mark.page@burdenandburden.co.uk>
2021-04-13 19:30:21 +0000

    Adds the file in which to build the public web service for pponce-api

---
+++ www/tickets_buy.snippet.php
@@ -0,0 +1,14 @@
+<?php
+
+require './bridge.php';
+require BLOTTO_WWW_FUNCTIONS;
+require BLOTTO_WWW_CONFIG;
+
+header ('Access-Control-Allow-Origin: *');
+
+// TODO
+// Provide Paypal one-off payment using BLOTTO_PAY_API_CLASS_PPONCE
+// See job specification
+// Steal and tweak the form code from git@dev.burdenandburden.co.uk:~/sss.git
+
+
