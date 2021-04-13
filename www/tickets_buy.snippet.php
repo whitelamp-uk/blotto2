5c30bb3642700661a406dcc6848da6189e732ddb
Mark Page <mark.page@burdenandburden.co.uk>
2021-04-13 19:33:30 +0000

    Adds some development notes

--- www/tickets_buy.snippet.php
+++ www/tickets_buy.snippet.php
@@ -8,7 +8,13 @@ header ('Access-Control-Allow-Origin: *');
 
 // TODO
 // Provide Paypal one-off payment using BLOTTO_PAY_API_CLASS_PPONCE
-// See job specification
+// See job specification and pponce-api/PayApi.class.php
+// Some back end work plus HTML output snippet
 // Steal and tweak the form code from git@dev.burdenandburden.co.uk:~/sss.git
+
+?>
+
+<form>
+</form>
 
 
