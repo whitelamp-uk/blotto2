5c64d9e6354a466070bbb9704fc4ea614d4da60e
Mark Page <mark.page@burdenandburden.co.uk>
2021-05-04 18:52:32 +0000

    Progresses callback script

--- www/callback.php
+++ www/callback.php
@@ -10,33 +10,72 @@ if (defined('VOODOOSMS') && VOODOOSMS) {
     require VOODOOSMS;
 }
 
+header ('Content-Type: text/plain');
+
 if (!array_key_exists('provider',$_GET)) {
     http_response_code (404);
+    echo "Resource not found\n";
     exit;
 }
 
 $apis = www_pay_apis ();
 if (!array_key_exists($_GET['provider'],$apis)) {
     http_response_code (404);
+    echo "Resource not found\n";
     exit;
 }
 
+$org_code       = BLOTTO_ORG_ID;
+$msg            = null;
+$responded      = false;
+$class          = null;
+
 try {
-    $file = $apis[$_GET['provider']]->file;
-    $class = $apis[$_GET['provider']]->class;
+    try {
+        $zo     = connect (BLOTTO_CONFIG_DB);
+        $msg    = $zo->query (
+          "
+            SELECT
+              signup_sms_message AS `msg`
+            FROM `blotto_org`
+            WHERE `org_code`=$org_code
+          "
+        );
+        $msg    = $msg->fetch_assoc()['msg'];
+    }
+    catch (\mysqli_sql_exception $e) {
+        throw new \Exception ("Could not get SMS message from database");
+    }
+    $file       = $apis[$_GET['provider']]->file;
+    $class      = $apis[$_GET['provider']]->class;
     require $file;
     // BLOTTO_MAKE_DB because data is permanent
-    $api = new $class (connect(BLOTTO_MAKE_DB));
-    $api->callback ();
+    $api        = new $class (connect(BLOTTO_MAKE_DB));
+    // $responded is boolean by reference
+    $api->callback ($msg,$responded);
 }
-catch (Exception $e) {
+catch (\Exception $e) {
+    echo "Something went wrong\n";
+    if ($responded) {
+        $responded = 'Y';
+    }
+    else {
+        $responded = 'N';
+        // Something went wrong before responding
+        // So respond now
+        http_response_code (500);
+    }
     error_log ('GET '.print_r($_GET,true));
     error_log ('POST '.print_r($_POST,true));
     error_log ($e->getMessage());
+    $subj = "Callback error from $class";
+    if ($class) {
+       $subj .= " class=$class";
+    }
     mail (
         BLOTTO_EMAIL_WARN_TO,
-        "Callback error from $class",
-        $e->getMessage(),
+        $subj,
+        $e->getMessage()."\nSent 200 OK? $responded",
         'From: '.BLOTTO_EMAIL_FROM
     );
 }
