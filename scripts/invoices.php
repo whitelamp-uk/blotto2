27ca864ef7296beca6a34d3caa7b80982e28f2b7
Mark Page <mark.page@burdenandburden.co.uk>
2021-08-15 08:48:13 +0000

    Improves invoice generation logic

--- scripts/invoices.php
+++ scripts/invoices.php
@@ -5,10 +5,12 @@ cfg ();
 require $argv[1];
 
 
-$zo = connect (BLOTTO_RESULTS_DB);
+$zo = connect (BLOTTO_MAKE_DB);
 if (!$zo) {
     exit (101);
 }
+
+$rdb = BLOTTO_RESULTS_DB;
 
 $first = '0000-00-00';
 if (defined('BLOTTO_INVOICE_FIRST') && BLOTTO_INVOICE_FIRST) {
@@ -17,18 +19,22 @@ if (defined('BLOTTO_INVOICE_FIRST') && BLOTTO_INVOICE_FIRST) {
 
 $qs = "
   SELECT
-    DISTINCT `draw_closed`
-  FROM `blotto_result`
-  WHERE `draw_closed`>='$first'
-  ORDER BY `draw_closed`
+    `r`.`draw_closed`
+   ,`w`.`ticket_number` IS NOT NULL AS `has_winners`
+  FROM `$rdb`.`blotto_result` AS `r`
+  LEFT JOIN `Wins` AS `w`
+         ON `w`.`draw_closed`=`r`.`draw_closed`
+  WHERE `r`.`draw_closed`>='$first'
+  GROUP BY `r`.`draw_closed`
+  ORDER BY `r`.`draw_closed`
   ;
 ";
 
-$draws = array ();
+$draws = [];
 try {
     $ds = $zo->query ($qs);
     while ($d=$ds->fetch_assoc()) {
-        $draws[] = $d['draw_closed'];
+        $draws[] = $d;
     }
 }
 catch (\mysqli_sql_exception $e) {
@@ -37,25 +43,27 @@ catch (\mysqli_sql_exception $e) {
 }
 
 try {
-    foreach ($draws as $draw_closed) {
+    foreach ($draws as $draw) {
         // Raise invoice for game services
         $file   = BLOTTO_DIR_INVOICE.'/';
-        $file  .= BLOTTO_ORG_USER.'_'.$draw_closed.'_game.html';
+        $file  .= BLOTTO_ORG_USER.'_'.$draw['draw_closed'].'_game.html';
         if (!file_exists($file)) {
-            if ($inv=invoice_game($draw_closed,false)) {
+            if ($inv=invoice_game($draw['draw_closed'],false)) {
                 $fp = fopen ($file,'w');
                 fwrite ($fp,$inv);
                 fclose ($fp);
             }
         }
         // Raise invoice (pass on cost) of paying out to winners
-        $file   = BLOTTO_DIR_INVOICE.'/';
-        $file  .= BLOTTO_ORG_USER.'_'.$draw_closed.'_payout.html';
-        if (!file_exists($file)) {
-            if ($inv=invoice_payout($draw_closed,false)) {
-                $fp = fopen ($file,'w');
-                fwrite ($fp,$inv);
-                fclose ($fp);
+        if ($draw['has_winners']) {
+            $file   = BLOTTO_DIR_INVOICE.'/';
+            $file  .= BLOTTO_ORG_USER.'_'.$draw['draw_closed'].'_payout.html';
+            if (!file_exists($file)) {
+                if ($inv=invoice_payout($draw['draw_closed'],false)) {
+                    $fp = fopen ($file,'w');
+                    fwrite ($fp,$inv);
+                    fclose ($fp);
+                }
             }
         }
     }
