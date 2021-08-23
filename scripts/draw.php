f96206dedf0e2429e43f59d6ca2ca00186b2a137
Mark Page <mark.page@burdenandburden.co.uk>
2021-08-23 21:21:42 +0000

    Fixes bug

--- scripts/draw.php
+++ scripts/draw.php
@@ -37,8 +37,8 @@
             [ "Winning number".plural(count($draw->results)).":", implode(', ',$draw->results) ],
             [ "Winners:", $draw->winners ],
             [ "Payout:", htmlspecialchars(BLOTTO_CURRENCY).number_format($draw->total,2) ],
-            [ "Players:", "$draw->players (last draw $draw->players_prv)" ],
-            [ "Tickets:", "$draw->tickets (last draw $draw->tickets_prv)" ]
+            [ "Players:", $players ],
+            [ "Tickets:", $tickets ]
         ],
         true
     );
