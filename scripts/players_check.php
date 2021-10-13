21bb5d2cb14774ae13422b3de211a6319857f5a8
Mark Page <mark.page@burdenandburden.co.uk>
2021-10-13 10:02:25 +0000

    Fixes broken player errors SQL

--- scripts/players_check.php
+++ scripts/players_check.php
@@ -13,29 +13,27 @@ if (!$zo) {
 // Final players check
 $qs = "
   SELECT
-    COUNT(`m`.`RefNo`) AS `total`
-   ,COUNT(`p`.`id` IS NULL) AS `missing`
-   ,COUNT(`p`.`chances` IS NULL)-COUNT(`p`.`id` IS NULL) AS `no_chances`
-   ,COUNT(`p`.`started` IS NULL OR `p`.`started`='0000-00-00')-COUNT(`p`.`id` IS NULL) AS `no_start_date`
-   ,GROUP_CONCAT(`m`.`ClientRef`) AS `refs`
-  FROM `blotto_build_mandate` AS `m`
+    `c`.`ClientRef`
+   ,COUNT(`c`.`DateDue`) AS `payments`
+   ,SUM(`c`.`PaidAmount`) AS `paid`
+   ,`p`.`id` AS `player_id`
+   ,`p`.`chances`
+   ,`p`.`started`
+  FROM `blotto_build_collection` AS `c`
   LEFT JOIN `blotto_player` AS `p`
-    ON `p`.`client_ref`=`m`.`ClientRef`
+    ON `c`.`ClientRef`=`p`.`client_ref`
   WHERE `p`.`chances` IS NULL
      OR `p`.`started` IS NULL
      OR `p`.`started`='0000-00-00'
+  GROUP BY `c`.`RefNo`
   ;
 ";
 try {
     $errors = $zo->query ($qs);
-    $errors = $errors->fetch_assoc ();
-    if ($errors['total']) {
-        fwrite (STDERR,"    ".$errors['total']." player errors:\n");
-        fwrite (STDERR,"    ".$errors['missing']." mandates are missing a player\n");
-        fwrite (STDERR,"    ".$errors['no_chances']." players have no chances\n");
-        fwrite (STDERR,"    ".$errors['no_start_date']." players have no start date\n");
-        fwrite (STDERR,$qs);
-        exit (102);
+    if ($errors->num_rows) {
+      fwrite (STDERR,$errors->num_rows." players have errors!\n");
+      fwrite (STDERR,$qs);
+      exit (102);
     }
 }
 catch (\mysqli_sql_exception $e) {
