1d2cdc23273602d1eb0c2879d0d3747a8370e905
Mark Page <mark.page@burdenandburden.co.uk>
2021-10-27 08:14:31 +0000

    Fixes bugs

--- scripts/anls.php
+++ scripts/anls.php
@@ -3,13 +3,13 @@
 require __DIR__.'/functions.php';
 cfg ();
 require $argv[1];
-require BLOTTO_STANNP_CLASS;
 
 
 try {
     if (defined('BLOTTO_STANNP') && BLOTTO_STANNP) {
         // Stannp API is active
         tee ("    Sending ANLs using Stannp\n");
+        require BLOTTO_STANNP_CLASS;
         $results = stannp_mail_anls ();
         tee ("      {$results['recipients']} mailpieces\n");
         print_r ($results);
--- scripts/wins.php
+++ scripts/wins.php
@@ -3,13 +3,13 @@
 require __DIR__.'/functions.php';
 cfg ();
 require $argv[1];
-require BLOTTO_STANNP_CLASS;
 
 
 try {
     if (defined('BLOTTO_STANNP') && BLOTTO_STANNP) {
         // Stannp API is active
         tee ("    Sending winner letters using Stannp\n");
+        require BLOTTO_STANNP_CLASS;
         $results = stannp_mail_wins ();
         tee ("      {$results['recipients']} mailpieces\n");
         print_r ($results);
