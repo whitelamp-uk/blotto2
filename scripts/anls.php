12798610e1851533a66b6a0627bac627654c2407
Mark Page <mark.page@burdenandburden.co.uk>
2022-02-13 12:35:07 +0000

    Separates Stannp active status for ANLs and winner letters

--- scripts/anls.php
+++ scripts/anls.php
@@ -8,16 +8,21 @@ require $argv[1];
 try {
     if (defined('BLOTTO_STANNP') && BLOTTO_STANNP) {
         // Stannp API is active
-        require BLOTTO_STANNP_CLASS;
-        tee ("    Updating ANLs.letter_status using Stannp\n");
-        stannp_status_anls ();
-        tee ("    Sending ANLs using Stannp\n");
-        $results = stannp_mail_anls ();
-        tee ("      {$results['recipients']} mailpieces\n");
-        print_r ($results);
+        if (defined('BLOTTO_STANNP_TPL_ANL') && BLOTTO_STANNP_TPL_ANL) {
+            require BLOTTO_STANNP_CLASS;
+            tee ("    Updating ANLs.letter_status using Stannp\n");
+            stannp_status_anls ();
+            tee ("    Sending ANLs using Stannp\n");
+            $results = stannp_mail_anls ();
+            tee ("      {$results['recipients']} mailpieces\n");
+            print_r ($results);
+        }
+        else {
+            tee ("    No postal API is active for ANLs\n");
+        }
     }
     else {
-        tee ("    No postal API is active for ANLs\n");
+        tee ("    No postal API is active\n");
     }
 }
 catch (\Exception $e) {
