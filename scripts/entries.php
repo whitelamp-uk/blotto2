0edd0fe5c9af242f14796ae14ba455a985e9c79c
Mark Page <mark.page@burdenandburden.co.uk>
2022-03-02 12:03:48 +0000

    Adds safety logic to guarantee that, where BLOTTO_INSURE is true, a ticket cannot be entered into a draw if it has not been insured for that draw close date

--- scripts/entries.php
+++ scripts/entries.php
@@ -8,6 +8,7 @@ tee ("    Draw entry engine\n");
 
 $ticket_db  = BLOTTO_TICKET_DB;
 $price      = BLOTTO_TICKET_PRICE;
+$org_ref    = strtoupper (BLOTTO_ORG_USER);
 $quiet      = get_argument('q',$Sw) !== false;
 $rbe        = get_argument('r',$Sw) !== false;
 if ($rbe && !function_exists('enter')) {
@@ -54,37 +55,20 @@ foreach ($close_dates as $date) {
         continue;
     }
 
-/*
-
-TODO: below is all very hacky
-
-We have bespoke PHP function:
-    draw_first ($first_collection_date,$ccc)
-which is no use in the SQL restrict-before-SUM() problem below:
-    `DateDue`<='not just one date'
-
-Which suggests we really to replace current PHP functions with SQL:
-    drawClosedFirst(collectedFirst,ccc) - org.bespoke.functions.sql
-    drawClosedFirstAsap(collectedFirst) - db.functions.sql
-    drawClosedFirstZaffoModel(collectedFirst) - db.functions.sql
-
-Then the SQL below can use drawClosedFirst() rather than
-  * rely on generic, un-customisable BLOTTO_PAY_DELAY
-  * have to hack the sum of PaidAmount as below
-
-Like this:
-    `DateDue` <= drawClosedFirst('$date',`blotto_supporter`.`canvas_code`)
-
-*/
-
     $date_dd = new DateTime ($date);
     $date_dd->sub (new DateInterval(BLOTTO_PAY_DELAY));  
     $date_dd = $date_dd->format ('Y-m-d');
+    // If we get to this point, the draw has already closed
+    // No insurance means no entry.
     $q = "
       SELECT
         `p`.`client_ref`
        ,`p`.`opening_balance` + `c_sum`.`PaidTotal` - IFNULL(`e_summary`.`paid_out`,0) AS `balance`
        ,GROUP_CONCAT(DISTINCT(`tk`.`number`)) AS `ticket_numbers`
+
+-- NEW INSURANCE BIT
+       ,COUNT(DISTINCT(`tk`.`number`))<=`i`.`tickets_insured` AS `is_insured`
+
       FROM `blotto_player` AS `p`
       LEFT JOIN (
         SELECT
@@ -101,6 +85,8 @@ Like this:
           FROM `blotto_build_collection`
 
 -- HORRIBLE HACK
+-- TODO: we need a better logical distinction between DD players and online players
+-- Freq='Single' was one idea but eventually we will support online players with repeat payments
           WHERE (`ClientRef` LIKE 'STRP%' AND `DateDue`<='$date')
              OR (`ClientRef` NOT LIKE 'STRP%' AND `DateDue`<='$date_dd')
 
@@ -109,6 +95,19 @@ Like this:
         ON `c_sum`.`ClientRef`=`p`.`client_ref`
       JOIN `$ticket_db`.`blotto_ticket` AS `tk`
         ON `tk`.`client_ref`=`p`.`client_ref`
+
+-- NEW INSURANCE BIT
+      LEFT JOIN (
+        SELECT
+          `client_ref`
+         ,COUNT(`ticket_number`) AS `tickets_insured`
+        FROM `blotto_insurance`
+        WHERE `org_ref`='$org_ref'
+          AND `draw_closed`='$date'
+        GROUP BY `client_ref`
+           ) AS `i`
+             ON `i`.`client_ref`=`tk`.`client_ref`
+
       WHERE `p`.`first_draw_close`<='$date'
       GROUP BY `p`.`id`
     ";
@@ -125,18 +124,28 @@ Like this:
         ";
         $n = 0;
         while ($r=$result->fetch_assoc()) {
+            $insured        = $r['is_insured'];
             $cref           = $r['client_ref'];
             $balance        = $r['balance'];
             $ticket_numbers = explode (',',$r['ticket_numbers']);
             $chances        = count ($ticket_numbers);
             if ($balance>=round($price*$chances/100,2)) {
-                foreach ($ticket_numbers as $key => $ticket_number) {
-                    $q     .= "( '$date', '$ticket_number', '$cref' ), ";
-                    $n++;
+
+// New insurance bit
+                if (!$insured && defined('BLOTTO_INSURE') && BLOTTO_INSURE) {
+                    fwrite (STDERR,"WARNING: '$date', refusing to enter uninsured player '{$r['client_ref']}'\n");
+                }
+                else {
+
+                    foreach ($ticket_numbers as $key => $ticket_number) {
+                        $q     .= "( '$date', '$ticket_number', '$cref' ), ";
+                        $n++;
+                    }
+
                 }
             }
             elseif ($balance<0) {
-                fwrite (STDERR,"ClientRef '$cref' has a negative balance!\n");
+                fwrite (STDERR,"WARNING: ClientRef '$cref' has a negative balance!\n");
             } 
         }
     }
