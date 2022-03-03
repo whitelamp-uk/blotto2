52af48b12235bea8f40a0c83665d44b0e940df3b
Mark Page <mark.page@burdenandburden.co.uk>
2022-03-03 10:44:27 +0000

    Fixes bug by calling new bespoke function draw_insuring()

--- scripts/exec.php
+++ scripts/exec.php
@@ -4,24 +4,29 @@ require __DIR__.'/functions.php';
 cfg ();
 require $argv[1];
 
+// At least a function name argument
 if (!array_key_exists(2,$argv)) {
     exit (121);
 }
 
+// Function must exist
 if (!function_exists($argv[2])) {
     exit (122);
 }
 
+// Must be a bespoke function
 $f = new ReflectionFunction ($argv[2]);
 if (realpath($f->getFileName())!=realpath(BLOTTO_BESPOKE_FUNC)) {
     exit (123);
 }
 
+// Execute
 $a = $argv;
 array_shift ($a);
 array_shift ($a);
 $f = array_shift ($a);
 
+// Return
 echo trim ($f(...$a)) ."\n";
 
 
