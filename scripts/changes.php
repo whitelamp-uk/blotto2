1c92f0cff24f280732e85de1546844c22d9f14c7
Mark Page <mark.page@burdenandburden.co.uk>
2022-03-04 19:38:49 +0000

    Reworks (and simplifies) code for generating changes

--- scripts/changes.php
+++ scripts/changes.php
@@ -6,293 +6,174 @@ require $argv[1];
 
 $cc_notify_interval = BLOTTO_CC_NOTIFY;
 
-function insert_changes ($zo,$changes) {
-    $rows = 0;
-    if (!is_array($changes)) {
-        fwrite (STDERR,"insert_changes(): not given an array\n");
-        exit (101);
-    }
-    if (!count($changes)) {
-        return $rows;
-    }
-    $insert = "
-      INSERT IGNORE INTO `blotto_change`
-      (`changed_date`,`ccc`,`agent_ref`,`canvas_ref`,`chance_number`,`client_ref_original`,`type`,`is_termination`,`reinstatement_for`,`amount_paid_before_this_date`,`supporter_signed`,`supporter_approved`,`supporter_created`,`supporter_first_paid`)
-      VALUES
-    ";
-    $count = 0;
-    $qi = $insert;
-    foreach ($changes as $c) {
-        $qi .= "('".implode("','",$c)."'),";
-        $count++;
-        if ($count==BLOTTO_ROWS_PER_QRY) {
-            try {
-                $zo->query (substr($qi,0,-1));
-                $rs = $zo->query ('SELECT ROW_COUNT() AS `rows`');
-                $rows += $rs->fetch_assoc()['rows'];
-            }
-            catch (\mysqli_sql_exception $e) {
-                fwrite (STDERR,$insert." ...\n".$e->getMessage()."\n");
-                exit (102);
-            }
-            $count = 0;
-            $qi = $insert;
-        }
-    }
-    if ($count) {
-        try {
-            $zo->query (substr($qi,0,-1));
-            $rs = $zo->query ('SELECT ROW_COUNT() AS `rows`');
-            $rows += $rs->fetch_assoc()['rows'];
-        }
-        catch (\mysqli_sql_exception $e) {
-            fwrite (STDERR,$insert." ...\n".$e->getMessage()."\n");
-            exit (102);
-        }
-    }
-    return $rows;
-}
-
-function push_changes (&$chs,$date,$qty_before,$qty_after,$amt,$s,$is_termination=0,$reinstatement_for='') {
-    if ($qty_before==$qty_after) {
-        return;
-    }
-    if ($qty_before<$qty_after) {
-        for ($i=$qty_before+1;$i<=$qty_after;$i++) {
-            array_push (
-                $chs,
-                array (
-                    $date,
-                    $s['ccc'],
-                    $s['agent_ref'],
-                    $s['canvas_ref'],
-                    $i,
-                    $s['client_ref_original'],
-                    'INC',
-                    $is_termination,
-                    $reinstatement_for,
-                    $amt,
-                    $s['supporter_signed'],
-                    $s['supporter_approved'],
-                    $s['supporter_created'],
-                    $s['supporter_first_paid']
-                )
-            );
-        }
-        return;
-    }
-    for ($i=$qty_before;$i>$qty_after;$i--) {
-        array_push (
-            $chs,
-            array (
-                $date,
-                $s['ccc'],
-                $s['agent_ref'],
-                $s['canvas_ref'],
-                $i,
-                $s['client_ref_original'],
-                'DEC',
-                $is_termination,
-                $reinstatement_for,
-                $amt,
-                $s['supporter_signed'],
-                $s['supporter_approved'],
-                $s['supporter_created'],
-                $s['supporter_first_paid']
-            )
-        );
-    }
-}
-
-
-
-echo "    Generating changes for canvassing companies\n";
+echo "-- New changes data for canvassing companies\n";
 
 $zo = connect (BLOTTO_MAKE_DB);
 if (!$zo) {
     exit (102);
 }
 
-
-// Get results from changesGenerate()
 $qs = "
   SELECT
-    *
-  FROM `tmp_changes_by_supporter`
-  ORDER BY `supporter_created` 
-  ;   
+    IFNULL(`cln1`.`updated`,`s`.`created`) AS `date_from`
+   ,`s`.`signed`
+   ,`s`.`approved`
+   ,`s`.`created`
+   ,`s`.`client_ref`
+   ,`s`.`canvas_code`
+   ,`s`.`canvas_agent_ref`
+   ,`s`.`canvas_ref`
+   ,`s`.`projected_chances` AS `chances_orig`
+   ,DATE_ADD(
+      IFNULL(`cln1`.`updated`,`s`.`created`)
+     ,INTERVAL $cc_notify_interval
+    ) AS `last_interested`
+   ,`u`.*
+   ,GROUP_CONCAT(CONCAT_WS('::',`p`.`id`,`p`.`chances`,`p`.`client_ref`) ORDER BY `p`.`id` SEPARATOR ';;') AS `players`
+  FROM `blotto_update` AS `u`
+  JOIN `blotto_supporter` AS `s`
+    ON `s`.`id`=`u`.`supporter_id`
+  JOIN `blotto_player` AS `p`
+    ON `p`.`supporter_id`=`s`.`id`
+   -- TODO: null chances implies non-DD/one-off/not CSV import or something
+   -- like one of those things - ie not relevant to CCR
+   AND `p`.`chances` IS NOT NULL
+  LEFT JOIN `blotto_update` AS `cln1`
+         ON `cln1`.`supporter_id`=`u`.`supporter_id`
+        AND `cln1`.`milestone`='first_collection'
+  -- Too much data to keep repeating oneself
+  LEFT JOIN (
+    SELECT
+      `update_id`
+    FROM `blotto_change`
+    GROUP BY `update_id`
+  )      AS `c`
+         ON `c`.`update_id`=`u`.`id`
+  WHERE `c`.`update_id` IS NULL
+  GROUP BY `u`.`id`
+  HAVING `u`.`updated`<=`last_interested`
+  ;
 ";
 try {
-    $ss = $zo->query ($qs);
+    $us = $zo->query ($qs);
 }
 catch (\mysqli_sql_exception $e) {
     fwrite (STDERR,$qs."\n".$e->getMessage()."\n");
-    exit (104);
-}
-$qs = "
-  SELECT
-    *
-  FROM `tmp_changes_termination`
-  ORDER BY `Cancelled_Date` 
-  ;   
-";
-try {
-    $ts = $zo->query ($qs);
-}
-catch (\mysqli_sql_exception $e) {
-    fwrite (STDERR,$qs."\n".$e->getMessage()."\n");
-    exit (105);
+    exit (103);
 }
 
-
-$supporters = [];
-$terminations = [];
-while ($s=$ss->fetch_assoc()) {
-    $ps = explode (';;',$s['players']);
-    $s['players'] = [];
-    foreach ($ps as $i=>$p) {
+$changes = [];
+while ($u=$us->fetch_assoc()) {
+    $u['player_old_chances'] = 0;
+    $u['player_chances'] = 0;
+    $u['is_termination'] = 0;
+    $u['player_client_ref'] = '';
+    $players = explode (';;',$u['players']);
+    foreach ($players as $i=>$p) {
         $p = explode ('::',$p);
-        if (!array_key_exists(5,$p)) {
-            fwrite (STDERR,"Player has too few properties: ".print_r($p,true)."\n");
-            exit (106);
+        if ($p[0]==$u['player_id']) {
+            $u['player_chances'] = $p[1];
+            $u['player_client_ref'] = $p[2];
+            break;
         }
-        $s['players'][$p[0]] = array (
-            'created' => $p[0],
-            'client_ref' => $p[1],
-            'chances' => $p[2],
-            'first_payment' => $p[3],
-            'last_payment' => $p[4],
-            'amount_paid' => $p[5]
-        );
+        $u['player_old_chances'] = $p[1];
     }
-    ksort ($s['players']);
-    array_push ($supporters,$s);
-}
-while ($t=$ts->fetch_assoc()) {
-    array_push ($terminations,$t);
-}
-
-
-$changes = [];
-echo "    ".count($supporters)." supporter changes\n";
-foreach ($supporters as $s) {
-    $chs                = 0;
-    $amount             = 0;
-    foreach ($s['players'] as $p) {
-        push_changes ($changes,$p['created'],$chs,$p['chances'],$amount,$s);
-        $chs            = $p['chances'];
-        $amount        += $p['amount_paid'];
+    if ($u['milestone']=='created') {
+        $u['increment'] = $u['chances_orig'];
+    }
+    elseif ($u['milestone']=='first_collection') {
+        // The actual first player chances is not synonoymous with sign-up chances
+        $u['increment'] = $u['player_chances'] - $u['chances_orig'];
+    }
+    elseif ($u['milestone']=='bacs_change') {
+        // Old player loses all old chances as new players gets some
+        $u['increment'] = $u['player_chances'] - $u['player_old_chances'];
+    }
+    elseif ($u['milestone']=='cancellation') {
+        $u['increment'] = 0 - $u['player_chances'];
+        $u['is_termination'] = 1;
+    }
+    elseif ($u['milestone']=='reinstatement') {
+        $u['increment'] = $u['player_chances'];
+    }
+    if ($u['increment']!=0) {
+        $count = abs ($u['increment']);
+        if ($u['increment']>0) {
+            $u['type_is_increment'] = 1;
+            $u['type'] = 'INC';
+            for ($i=0;$i<$count;$i++) {
+                $u['chance_number'] = $i + 1;
+                $u['chance_ref'] = $u['canvas_ref'].'-'.$u['chance_number'];
+                array_push ($changes,$u);
+            }
+        }
+        else {
+            $u['type_is_increment'] = 0;
+            $u['type'] = 'DEC';
+            for ($i=0;$i<$count;$i++) {
+                $u['chance_number'] = $count - $i;
+                $u['chance_ref'] = $u['canvas_ref'].'-'.$u['chance_number'];
+                array_push ($changes,$u);
+            }
+        }
+        $qs = "
+          SELECT
+            SUM(`PaidAmount`) AS `amount_paid_at_update`
+          FROM `blotto_build_collection`
+          WHERE `ClientRef`='{$u['player_client_ref']}'
+            AND `DateDue`<='{$u['date_from']}'
+          ;
+        ";
+        try {
+            $amt = $zo->query ($qs);
+            $u['amount_paid_at_update'] = $amt->fetch_assoc() ['amount_paid_at_update'];
+        }
+        catch (\mysqli_sql_exception $e) {
+            fwrite (STDERR,$qs."\n".$e->getMessage()."\n");
+            exit (104);
+        }
+    }
+    else {
+        // This does sometimes happen eg:
+        // DBH BB6919_163438 x1  --->  BB6919_163438-0001 x1
     }
 }
-echo "    ".count($terminations)." termination changes\n";
-foreach ($terminations as $t) {
-    push_changes ($changes,$t['Cancelled_Date'],$t['chances'],0,$t['amount_paid'],$t,1);
+
+
+if (!count($changes)) {
+    fwrite (STDERR,"No change data to write to blotto_change\n");
+    exit (0);
 }
-echo "    ".insert_changes($zo,$changes)." new supporter/termination rows (1 per chance)\n";
 
-
-/*
-    If a change with is_termination > 0:
-     * has related collection(s) after its change date
-     * is not related to a reinstatement change
-           then it needs one inserting with:
-            * change date = first post-termination collection date
-            * reinstatement_for = change date of the termination
-*/
-
-$qs = "
-  SELECT
-    `t`.*
-   ,MIN(`r`.`DateDue`) AS `reinstatement_date`
-  FROM (
-    SELECT
-      `ct`.`changed_date`
-     ,`ct`.`ccc`
-     ,`ct`.`agent_ref`
-     ,`ct`.`canvas_ref`
-     ,MAX(`ct`.`chance_number`) AS `chances` 
-     ,`ct`.`client_ref_original`
-     ,`ct`.`supporter_signed`
-     ,`ct`.`supporter_approved`
-     ,`ct`.`supporter_created`
-     ,`ct`.`supporter_first_paid`
-     ,`ct`.`amount_paid_before_this_date`
-    FROM `blotto_change` AS `ct`
-    LEFT JOIN `blotto_change` AS `cr`
-           ON `cr`.`ccc`=`ct`.`ccc`
-          AND `cr`.`canvas_ref`=`ct`.`canvas_ref`
-          AND `cr`.`chance_number`=`ct`.`chance_number`
-          AND `cr`.`reinstatement_for`=`ct`.`changed_date`
-    WHERE `ct`.`is_termination`>0
-      AND `cr`.`changed_date` IS NOT NULL
-    GROUP BY `ct`.`changed_date`,`ct`.`ccc`,`ct`.`canvas_ref`
-  ) AS `t`
-  JOIN (
-    SELECT
-      `s`.`client_ref` AS `client_ref_original`
-     ,`c`.`DateDue`
-    FROM `blotto_supporter` AS `s`
-    JOIN `blotto_player` AS `p`
-      ON `p`.`supporter_id`=`s`.`id`
-    JOIN `blotto_build_collection` AS `c`
-      ON `c`.`ClientRef`=`p`.`client_ref`
-  ) AS `r`
-    ON `r`.`client_ref_original`=`t`.`client_ref_original`
-   AND `r`.`DateDue`<=DATE_ADD(
-          IF(
-            `t`.`supporter_first_paid` IS NULL
-           ,`t`.`supporter_created`
-           ,`t`.`supporter_first_paid`
-           )
-          ,INTERVAL $cc_notify_interval
-        )
-   AND `r`.`DateDue`>=`t`.`changed_date`
-  GROUP BY `t`.`changed_date`,`t`.`ccc`,`t`.`canvas_ref`
-  ;
+echo "
+INSERT INTO `blotto_change` (
+  `changed_date`
+ ,`client_ref`
+ ,`ccc`
+ ,`canvas_agent_ref`
+ ,`signed`
+ ,`approved`
+ ,`created`
+ ,`canvas_ref`
+ ,`chance_number`
+ ,`chance_ref`
+ ,`type`
+ ,`type_is_increment`
+ ,`is_termination`
+ ,`chances_orig`
+ ,`supporter_id`
+ ,`update_id`
+ ,`milestone`
+) VALUES
 ";
 
-try {
-    $cs = $zo->query ($qs);
-}
-catch (\mysqli_sql_exception $e) {
-    fwrite (STDERR,$qs."\n".$e->getMessage()."\n");
-    exit (107);
-}
-
-
-$changes = [];
-while ($c=$cs->fetch_assoc()) {
-    push_changes ($changes,$c['reinstatement_date'],0,$c['chances'],$c['amount_paid_before_this_date'],$c,0,$c['changed_date']);
-}
-echo "    ".count($changes)." reinstatement changes\n";
-echo "    ".insert_changes($zo,$changes)." new reinstatement rows (1 per chance)\n";
-
-$qd = "
-  DELETE FROM `blotto_change`
-  WHERE `changed_date`='0000-00-00'
-     OR `changed_date` IS NULL
-  ;
-";
-try {
-    $zo->query ($qd);
-}
-catch (\mysqli_sql_exception $e) {
-    fwrite (STDERR,$qd."\n".$e->getMessage()."\n");
-    exit (108);
+foreach ($changes as $i=>$c) {
+    echo "('{$c['updated']}','{$c['client_ref']}','{$c['canvas_code']}','{$c['canvas_agent_ref']}','{$c['signed']}','{$c['approved']}','{$c['created']}','{$c['canvas_ref']}',{$c['chance_number']},'{$c['chance_ref']}','{$c['type']}',{$c['type_is_increment']},{$c['is_termination']},{$c['chances_orig']},{$c['supporter_id']},{$c['id']},'{$c['milestone']}')";
+    if (array_key_exists($i+1,$changes)) {
+        echo ",";
+    }
+    echo "\n";
 }
 
-$qu = "
-  UPDATE `blotto_change`
-  SET
-    `supporter_first_paid`=null
-  WHERE `supporter_first_paid`='0000-00-00'
-  ;
-";
-try {
-    $zo->query ($qu);
-}
-catch (\mysqli_sql_exception $e) {
-    fwrite (STDERR,$qu."\n".$e->getMessage()."\n");
-    exit (109);
-}
+echo ";\n\n";
 
