8629bcd05128c2b759f73fdf1851af18a208e507
Mark Page <mark.page@burdenandburden.co.uk>
2023-03-28 12:23:34 +0000

    Improves www config example

--- scripts/example.cfg.www.php
+++ scripts/example.cfg.www.php
@@ -1,8 +1,8 @@
 <?php
 
-define ( 'BLOTTO_WWW_COOKIE_PATH',      '/mark/blotto/abc'                                  );
-define ( 'BLOTTO_WWW_FUNCTIONS',        '/home/mark/blotto/blotto/scripts/functions.php'    );
-define ( 'BLOTTO_WWW_CONFIG',           '/home/mark/blotto/config/abc.cfg.php'              );
+define ( 'BLOTTO_WWW_COOKIE_PATH',      '/mark/blotto/ylh'                                  );
+define ( 'BLOTTO_WWW_FUNCTIONS',        'mylotto/blotto2/scripts/functions.php'             );
+define ( 'BLOTTO_WWW_CONFIG',           'mylotto/config/ylh.cfg.php'                        );
 define ( 'BLOTTO_WWW_KEY',              '5,=aNClQ;elCdjvb+#wAN#g+Nm|ph+!hvZRPU:6fe%9-B`6bL4jmG,.t2y:w>H2a' );
 define ( 'BLOTTO_WWW_IV',               'r>qPm -6bAz&nC&fm--Bt6 W4i~}/cso:QC4qo]Zt+UXhIN(+-xi*@/+n_SWQ#R:' );
 define ( 'BLOTTO_WWW_ALG',              'AES-256-CBC'                                       );
@@ -10,5 +10,5 @@ define ( 'BLOTTO_WWW_SESSION_MINUTES',  15                                      
 define ( 'BLOTTO_WWW_ROWS_MAX_VIEW',    2500                                                );
 define ( 'BLOTTO_WWW_ROWS_MAX_PULL',    100000                                              );
 define ( 'BLOTTO_WWW_FACEBOOK_ID',      null                                                );
-define ( 'BLOTTO_WWW_IFRAME_SOURCES',   '\'self\' https://dbhc.org.uk http://dbhc.org.uk'   );
+define ( 'BLOTTO_WWW_IFRAME_SOURCES',   '\'self\' https://ylh.org.uk http://ylh.org.uk'     );
 
