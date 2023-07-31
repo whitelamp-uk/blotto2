6974773d30338cb440ed2f915a94264b9c342af6
Mark Page <mark.page@burdenandburden.co.uk>
2023-07-31 11:57:43 +0000

    Adds per-digit spans to results numbers so that CSS may be used to create "balls"

--- www/media/winners.js
+++ www/media/winners.js
@@ -2,7 +2,7 @@
 
 (
     function () {
-        var format,url,xhttp;
+        var ball,balls,format,i,num,nums,url,xhttp;
         format          = document.getElementById ('lottery-winners-latest');
         if (!format) {
             return;
@@ -14,6 +14,17 @@
                 var element;
                 element = document.getElementById ('lottery-winners-latest');
                 element.innerHTML = xhttp.responseText;
+                nums = document.body.querySelectorAll ('#lottery-results-latest-table tbody td:nth-child(2)');
+                for (num of nums) {
+                    balls = num.textContent;
+                    num.textContent = '';
+                    for (i=0;i<balls.length;i++) {
+                        ball = document.createElement ('span');
+                        ball.classList.add ('ball');
+                        ball.textContent = balls.charAt (i);
+                        num.appendChild (ball);
+                    }
+                }
             }
         };
         // Date format: https://www.php.net/manual/en/datetime.format.php
