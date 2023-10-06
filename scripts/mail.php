0ab031ed0d4e6bdf10bcdd33a23d319eda199d30
Mark Page <mark.page@burdenandburden.co.uk>
2023-10-06 15:33:38 +0000

    Fixes bug (removes temporary diagnostic)

--- scripts/mail.php
+++ scripts/mail.php
@@ -28,7 +28,7 @@ $body = "Message$warning at ".date('Y-m-d H:i:s')."\n".$argv[2]."\n";
 if (defined('BLOTTO_DEV_PAY_FREEZE') && BLOTTO_DEV_PAY_FREEZE) {
     $body .= "Warning: BLOTTO_DEV_PAY_FREEZE=true so no payment data was processed and no cancellations were calculated.\n";
 }
-echo $body; exit;
+
 mail (
     BLOTTO_EMAIL_WARN_TO,
     BLOTTO_BRAND." - Status report for ".BLOTTO_ORG_NAME." from ".BLOTTO_MC_NAME,
