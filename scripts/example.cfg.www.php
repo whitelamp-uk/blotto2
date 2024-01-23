16c15c5b6614f8c5acb984e40977002e53f1ce59
Mark Page <mark.page@burdenandburden.co.uk>
2024-01-23 14:12:58 +0000

    Adds password reset definitions BLOTTO_WWW_PWDRST_* to example web config

--- scripts/example.cfg.www.php
+++ scripts/example.cfg.www.php
@@ -11,4 +11,9 @@ define ( 'BLOTTO_WWW_ROWS_MAX_VIEW',    2500                                    
 define ( 'BLOTTO_WWW_ROWS_MAX_PULL',    100000                                              );
 define ( 'BLOTTO_WWW_FACEBOOK_ID',      null                                                );
 define ( 'BLOTTO_WWW_IFRAME_SOURCES',   '\'self\' https://ylh.org.uk http://ylh.org.uk'     );
-
+define ( 'BLOTTO_WWW_PWDRST_MINS',      5                                                   );
+define ( 'BLOTTO_WWW_PWDRST_SMS_NAME',  'MyELMsvc'                                          );
+define ( 'BLOTTO_WWW_PWDRST_VFY_CM_ID', '1-4a91b-bd3e-a958e236ce4-6ebc2605477'              );
+define ( 'BLOTTO_WWW_PWDRST_VFY_SMS',   'should be entered into the password reset form. If you did not request this message you can safely ignore it' );
+define ( 'BLOTTO_WWW_PWDRST_NTY_CM_ID', '211e-1d4cf1-ba69b1-47b632d2e-4106dab'              );
+define ( 'BLOTTO_WWW_PWDRST_NTY_SMS',   'Your ELM password has been changed. If you did not do this, please report to the administrator immediately on 01234098765.' );
