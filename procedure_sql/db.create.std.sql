6086938bc98506e60432f77accf3a18688c34d76
Mark Page <mark.page@burdenandburden.co.uk>
2025-05-16 10:48:04 +0000

    Ensures that mandate and collection tables always exist

--- procedure_sql/db.create.std.sql
+++ procedure_sql/db.create.std.sql
@@ -6,6 +6,59 @@ USE `{{BLOTTO_MAKE_DB}}`
 SET foreign_key_checks=0;
 
 
+CREATE TABLE IF NOT EXISTS `blotto_build_collection` (
+  `DateDue` date DEFAULT NULL,
+  `Provider` char(4) CHARACTER SET ascii DEFAULT NULL,
+  `RefNo` bigint(20) unsigned DEFAULT NULL,
+  `RefOrig` varchar(64) CHARACTER SET ascii DEFAULT NULL,
+  `ClientRef` varchar(255) CHARACTER SET ascii DEFAULT NULL,
+  `PaidAmount` decimal (10,2) DEFAULT NULL,
+  UNIQUE KEY `DateDue_Provider_RefOrig` (`DateDue`,`Provider`,`RefOrig`),
+  UNIQUE KEY `DateDue_ClientRef` (`DateDue`,`ClientRef`),
+  KEY `DateDue` (`DateDue`),
+  KEY `RefNo` (`RefNo`),
+  KEY `RefOrig` (`RefOrig`),
+  KEY `ClientRef` (`ClientRef`),
+  KEY `PaidAmount` (`PaidAmount`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+DROP TABLE IF EXISTS `blotto_build_mandate`;
+
+CREATE TABLE IF NOT EXISTS `blotto_build_mandate` (
+  `Provider` char(4) CHARACTER SET ascii DEFAULT NULL,
+  `RefNo` bigint(20) unsigned DEFAULT NULL,
+  `RefOrig` varchar(64) CHARACTER SET ascii DEFAULT NULL,
+  `ClientRef` varchar(255) CHARACTER SET ascii DEFAULT NULL,
+  `Created` date DEFAULT NULL,
+  `Updated` date DEFAULT NULL,
+  `StartDate` char(16) CHARACTER SET ascii DEFAULT NULL,
+  `Status` char(16) CHARACTER SET ascii DEFAULT NULL,
+  `Freq` char(16) CHARACTER SET ascii DEFAULT NULL,
+  `Amount` decimal(10,2) DEFAULT NULL,
+  `ChancesCsv` varchar(255) CHARACTER SET ascii NOT NULL,
+  `Name` varchar(255) CHARACTER SET ascii DEFAULT NULL,
+  `Sortcode` char(16) CHARACTER SET ascii DEFAULT NULL,
+  `Account` char(16) CHARACTER SET ascii DEFAULT NULL,
+  `FailReason` varchar(255) CHARACTER SET ascii DEFAULT NULL,
+  `ImportId` int(10) unsigned DEFAULT NULL,
+  `TimesCreated` tinyint(3) unsigned DEFAULT NULL,
+  `LastCreated` date DEFAULT NULL,
+  `LastStartDate` char(16) CHARACTER SET ascii DEFAULT NULL,
+  UNIQUE KEY `Provider_RefOrig` (`Provider`,`RefOrig`),
+  UNIQUE KEY `ClientRef` (`ClientRef`),
+  KEY `Created` (`Created`),
+  KEY `Updated` (`Updated`),
+  KEY `StartDate` (`StartDate`),
+  KEY `Status` (`Status`),
+  KEY `Freq` (`Freq`),
+  KEY `Amount` (`Amount`),
+  KEY `TimesCreated` (`TimesCreated`),
+  KEY `LastCreated` (`LastCreated`),
+  KEY `LastStartDate` (`LastStartDate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
 CREATE TABLE IF NOT EXISTS `blotto_change` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `changed_date` date NULL,
