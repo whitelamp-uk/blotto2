9b8f7419cc8554681c4636f95330bd6a73fbc11a
Mark Page <mark.page@burdenandburden.co.uk>
2025-07-03 11:52:49 +0000

    Adds winners API HTML data attritute data-matchnumtitle

--- www/media/winners.js
+++ www/media/winners.js
@@ -3,17 +3,28 @@
 (
     function () {
         var ball,balls,format,i,num,nums,url,xhttp;
+var matchnumcell,matchnumtitle;
         format          = document.getElementById ('lottery-winners-latest');
         if (!format) {
             return;
         }
+if (format.dataset.matchnumtitle) {
+    matchnumtitle = format.dataset.matchnumtitle;
+}
         format          = format.dataset.dateformat;
         xhttp           = new XMLHttpRequest ();
         xhttp.onreadystatechange = function ( ) {
             if (this.readyState==4 && this.status==200) {
                 var element;
                 element = document.getElementById ('lottery-winners-latest');
+element.style.visibility = 'hidden';
                 element.innerHTML = xhttp.responseText;
+if (matchnumtitle) {
+    matchnumcell = element.querySelector ('#lottery-results-latest-table th:first-of-type')
+    if (matchnumcell) {
+        matchnumcell.textContent = matchnumtitle;
+    }
+}
                 nums = document.body.querySelectorAll ('#lottery-results-latest-table tbody td:nth-child(2)');
                 for (num of nums) {
                     balls = num.textContent;
@@ -25,6 +36,7 @@
                         num.appendChild (ball);
                     }
                 }
+element.style.visibility = 'visible';
             }
         };
         // Date format: https://www.php.net/manual/en/datetime.format.php
