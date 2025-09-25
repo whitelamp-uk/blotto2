da9363d236c79f6a40d9e349630a873a45906d38
Dom Latter <dom.latter@burdenandburden.co.uk>
2025-09-25 13:28:12 +0000

    Suppress PHP Notice if no draws to display. Simplify variable handling

--- www/winners.snippet.php
+++ www/winners.snippet.php
@@ -6,37 +6,32 @@ require BLOTTO_WWW_CONFIG;
 
 header ('Access-Control-Allow-Origin: *');
 
-$f = null;
-if (array_key_exists('f',$_GET)) {
-    $f = $_GET['f'];
-    if (!$f) {
-        $f = null;
-    }
-}
+$f = empty($_GET['f']) ? null : $_GET['f'];
 
 $w = www_winners ($f);
 
-table (
-    'lottery-winners-latest-table',
-    '',
-    'Winners for '.$w->date,
-    ['Ticket','Prize'],
-    $w->winners,
-    true,
-    false,
-    $w->classes->winners
-);
-
-if ($w->number_matches && count($w->number_matches)) {
+if (isset($w->date)) {
     table (
-        'lottery-results-latest-table',
+        'lottery-winners-latest-table',
         '',
-        'Draw results for '.$w->date,
-        ['Prize','Number'],
-        $w->number_matches,
+        'Winners for '.$w->date,
+        ['Ticket','Prize'],
+        $w->winners,
         true,
         false,
-        $w->classes->number_matches
+        $w->classes->winners
     );
-}
 
+    if ($w->number_matches && count($w->number_matches)) {
+        table (
+            'lottery-results-latest-table',
+            '',
+            'Draw results for '.$w->date,
+            ['Prize','Number'],
+            $w->number_matches,
+            true,
+            false,
+            $w->classes->number_matches
+        );
+    }
+}
