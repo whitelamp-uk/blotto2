1e463b3e74f020176e265c5f062fe2aa6d289197
Dom Latter <dom.latter@burdenandburden.co.uk>
2025-11-04 18:13:51 +0000

    Have whole number of pounds when value changes (i.e. £10 not £10-00)

--- www/media/signup.js
+++ www/media/signup.js
@@ -26,7 +26,7 @@ function drawsHandle (evt) {
     cost2   = document.querySelector ('form.signup #signup-cost-confirm > output');
     ppt     = document.querySelector ('form.signup [data-ppt]');
     ppt     = 1 * ppt.dataset.ppt;
-    cost.textContent = (tickets*evt.target.value*ppt).toFixed(2).replace ('.',cost.dataset.decsepchar);
+    cost.textContent = (tickets*evt.target.value*ppt); //.toFixed(2).replace ('.',cost.dataset.decsepchar);
     cost2.textContent = cost.textContent;
 }
 
