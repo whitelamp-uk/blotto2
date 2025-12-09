fd649972a67e98636493c92e4cc843569ac49d8c
castle <castle@burdenandburden.co.uk>
2025-12-09 13:01:59 +0000

    Bump tickets widget CSS version to refresh client cache

--- www/ticketsc.php
+++ www/ticketsc.php
@@ -479,7 +479,8 @@ $contact_phone = org()['admin_phone'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title title="Buy tickets now">Buy tickets now</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./ticketsc/ticketsc.css">
+    <!-- <link rel="stylesheet" href="./ticketsc/ticketsc.css"> -->
+    <link rel="stylesheet" href="./ticketsc/ticketsc.css?v=20251209">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- <script type="text/javascript" src="./ticketsc/js-config.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
