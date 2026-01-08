007e799541135a88a70bc97dc299f992deed7fd5
Dom Latter <dom.latter@burdenandburden.co.uk>
2026-01-08 18:36:44 +0000

    tickets.php modified to actually run through the results of dbs() tickets_insert.php added as a replacement to insert directly into ticket db

--- scripts/tickets.php
+++ scripts/tickets.php
@@ -4,18 +4,12 @@ require __DIR__.'/functions.php';
 cfg ();
 require $argv[1];
 
-
 $zo = connect (BLOTTO_MAKE_DB);
 if (!$zo) {
     exit (101);
 }
 
-
-$org = BLOTTO_ORG_NAME;
-$fdb = BLOTTO_DB;
 $tdb = BLOTTO_TICKET_DB;
-$dbs = [];
-
 
 if (!BLOTTO_TICKETS_AUTO) {
     tee ("-- Auto-assignment of tickets is switched off - skipping\n");
@@ -23,8 +17,6 @@ if (!BLOTTO_TICKETS_AUTO) {
 }
 
 try {
-
-
     $dbs                = dbs ();
     tee ("-- Ticket pool is in `$tdb`.`blotto_ticket`\n");
 
@@ -32,66 +24,67 @@ try {
     echo "SET @today = CURDATE();\n\n";
 
     $pad_length         = strlen (BLOTTO_TICKET_MAX);
-    $count              = 0;
-    $tickets            = [];
-    $players            = [];
-    $qty                = 0;
 
     foreach ($dbs as $org_id=>$db) {
+        $tickets            = [];
+        $players            = [];
+        $qty                = 0;
         players_new ($players,$qty,$org_id,$db);
         tee ("-- $qty tickets required for org #$org_id `{$db['make']}`\n");
         if ($qty==0) {
             continue;
         }
-    }
-
-    for ($i=0;$i<$qty;$i++) {
-        while (1) {
-            $new = mt_rand (intval(BLOTTO_TICKET_MIN),intval(BLOTTO_TICKET_MAX));
-            $new = str_pad ($new,$pad_length,'0',STR_PAD_LEFT);
-            if (in_array($new,$tickets)) {
-                // Already selected so try again
-                continue;
-            }
-            $qs = "
-              SELECT
-                `number`
-              FROM `$tdb`.`blotto_ticket`
-              WHERE `number`='$new'
-              LIMIT 0,1
-              ;
-            ";
-            try {
-                $r = $zo->query ($qs);
-                if ($r->num_rows>0) {
-                    // Already issued so try again
+        // find available ticket numbers
+        for ($i=0;$i<$qty;$i++) {
+            while (1) {
+                $new = mt_rand (intval(BLOTTO_TICKET_MIN),intval(BLOTTO_TICKET_MAX));
+                $new = str_pad ($new,$pad_length,'0',STR_PAD_LEFT);
+                if (in_array($new,$tickets)) {
+                    // Already selected so try again
                     continue;
                 }
+                $qs = "
+                  SELECT
+                    `number`
+                  FROM `$tdb`.`blotto_ticket`
+                  WHERE `number`='$new'
+                  LIMIT 0,1
+                  ;
+                ";
+                try {
+                    $r = $zo->query ($qs);
+                    if ($r->num_rows>0) {
+                        // Already issued so try again
+                        continue;
+                    }
+                }
+                catch (\mysqli_sql_exception $e) {
+                    fwrite (STDERR,$qs."\n".$e->getMessage()."\n");
+                    exit (102);
+                }
+                $tickets[] = $new;
+                break;
             }
-            catch (\mysqli_sql_exception $e) {
-                fwrite (STDERR,$qs."\n".$e->getMessage()."\n");
-                exit (102);
+        }
+
+        // Safety check
+        if (count($tickets)!=$qty) {
+            fwrite (STDERR,count($tickets)." selected which is different to $qty needed\n");
+            exit (103);
+        }
+
+        // Generate SQL
+        foreach ($players as $p) {
+            $crf        = escm ($p['ClientRef']);
+            $rno        = escm ($p['RefNo']);
+            for ($i=0;$i<$p['qty'];$i++) {
+                $tkt    = array_pop ($tickets); // pop not shift!
+                echo "INSERT INTO `blotto_ticket` SET `org_id`={$p['org_id']},`mandate_provider`='{$p['Provider']}',`dd_ref_no`='$rno',`client_ref`='$crf',`issue_date`=@today,`number`='{$tkt}';\n";
             }
-            $tickets[] = $new;
-            break;
         }
     }
 
-    // Safety check
-    if (count($tickets)!=$qty) {
-        fwrite (STDERR,count($tickets)." selected which is different to $qty needed\n");
-        exit (103);
-    }
 
-    // Generate SQL
-    foreach ($players as $p) {
-        $crf        = escm ($p['ClientRef']);
-        $rno        = escm ($p['RefNo']);
-        for ($i=0;$i<$p['qty'];$i++) {
-            $tkt    = array_shift ($tickets);
-            echo "INSERT INTO `blotto_ticket` SET `org_id`={$p['org_id']},`mandate_provider`='{$p['Provider']}',`dd_ref_no`='$rno',`client_ref`='$crf',`issue_date`=@today,`number`='{$tkt}';\n";
-        }
-    }
 
 }
 catch (\Exception $e) {
---
+++ scripts/tickets_insert.php
@@ -0,0 +1,72 @@
+<?php
+
+require __DIR__.'/functions.php';
+cfg ();
+require $argv[1];
+
+if (!BLOTTO_TICKETS_AUTO) {
+    tee ("-- Auto-assignment of tickets is switched off - skipping\n");
+    exit (0);
+}
+
+$zo = connect (BLOTTO_MAKE_DB);
+if (!$zo) {
+    exit (101);
+}
+
+try {
+    $dbs                = dbs (); // in practice, only one
+
+    $pad_length         = strlen (BLOTTO_TICKET_MAX);
+
+    foreach ($dbs as $org_id=>$db) {
+        $tickets            = [];
+        $players            = [];
+        $qty                = 0;
+        players_new ($players,$qty,$org_id,$db);
+        //tee ("-- $qty tickets required for org #$org_id `{$db['make']}`\n");
+        if ($qty==0) {
+            continue;
+        }
+
+        foreach ($players as $p) {
+            $org_id    =  escm ($p['org_id']);
+            $Provider  =  escm ($p['Provider']);
+            $RefNo     = escm ($p['RefNo']);
+            $ClientRef = escm ($p['ClientRef']);
+            $inserted = 0;
+            while ($inserted<$p['qty']) {
+                // a bit clearer in PHP than lpad(floor(rand())) in sql
+                $new = mt_rand (intval(BLOTTO_TICKET_MIN),intval(BLOTTO_TICKET_MAX));
+                $new = str_pad ($new,$pad_length,'0',STR_PAD_LEFT);
+
+                $qi = "
+                  INSERT INTO `BLOTTO_TICKET_DB`.`blotto_ticket` SET
+                    `number`='$new'
+                   ,`issue_date`=CURDATE()
+                   ,`org_id`='$org_id'
+                   ,`mandate_provider`='$Provider'
+                   ,`dd_ref_no`='$RefNo'
+                   ,`client_ref`='$ClientRef'
+                  ON DUPLICATE KEY UPDATE `number`=`number`
+                  ;
+                ";
+                try {
+                    $zo->query ($qi);
+                    if ($zo->affected_rows > 0) {  // if new number inserted
+                        $inserted++;
+                    }
+                }
+                catch (\mysqli_sql_exception $e) {
+                    fwrite (STDERR,$qs."\n".$e->getMessage()."\n");
+                    exit (102);
+                }
+            }
+        }
+    }
+}
+catch (\Exception $e) {
+    fwrite (STDERR,$e->getMessage()."\n");
+    exit (104);
+}
+
